049e438e46f7869039028673c58d1ca0af93a4b3
Lisogorsky <lisogorsky@mail.ru>
2017-07-21 22:16:00 +0300

    correct stazh prepodavateley

--- head.php
+++ head.php
@@ -16,3 +16,66 @@
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
+
+<? 
+# педагогический стаж
+$viktorovaE_ped = 26;
+$radosteva_ped = 43;
+$aleksandrova_ped = 11;
+$andreeva_ped = 2;
+$baharevich_ped = 43;
+$viktorovaM_ped = 6;
+$volkova_ped = 4;
+$dmitrieva_ped = 36;
+$zhitinkina_ped = 33;
+$inorodtseva_ped = 5;
+$kozhevnikova_ped = 40;
+$kuzmihyh_ped = 3;
+$lezhnina_ped = 31;
+$lisogorskaya_ped = 38;
+$magsumyanova_ped = 30;
+$maslenikova_ped = 14;
+$matukov_ped = 2;
+$popova_ped = 43;
+$semenova_ped = 4;
+$sundyrevaEl_ped = 33;
+$sundyrevaEv_ped = 6;
+$sundyrev_ped = 9;
+$filimonova_ped = 6;
+$formozov_ped = 29;
+$frantsek_ped = 22;
+$hripunova_ped = 31;
+$emelianova_ped = 39;
+$trushnikova_ped = 4;
+$shishkareva_ped = 38;
+
+# общий стаж
+$viktorovaE_full = 26;
+$radosteva_full = 43;
+$aleksandrova_full = 17;
+$andreeva_full = 2;
+$baharevich_full = 43;
+$viktorovaM_full = 6;
+$volkova_full = 6;
+$dmitrieva_full = 36;
+$zhitinkina_full = 33;
+$inorodtseva_full = 5;
+$kozhevnikova_full = 40;
+$kuzmihyh_full = 3;
+$lezhnina_full = 31;
+$lisogorskaya_full = 38;
+$magsumyanova_full = 34;
+$maslenikova_full = 21;
+$matukov_full = 2;
+$popova_full = 43;
+$semenova_full = 4;
+$sundyrevaEl_full = 33;
+$sundyrevaEv_full = 6;
+$sundyrev_full = 9;
+$filimonova_full = 29;
+$formozov_full = 36;
+$frantsek_full = 29;
+$hripunova_full = 33;
+$emelianova_full = 39;
+$trushnikova_full = 4;
+$shishkareva_full = 38;
